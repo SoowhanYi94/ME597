c2797bd0204a5a05be49fe69f27a474bff8273ff
soowhanyi <soowhanyi94@gmail.com>
2024-02-09 21:28:20 -0800

    4

--- HW4/HW4/p4.py
+++ HW4/HW4/p4.py
@@ -4,16 +4,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 from mpl_toolkits import mplot3d
+import math
+
+
+
+def show(graph): 
+    plt.figure()
+    poses = nx.get_node_attributes(graph, 'pos')
+    nx.draw_networkx_edges(graph, pos = poses,edgelist=graph.edges(),arrows=True)
+    nx.draw_networkx_nodes(graph, pos = poses, nodelist=graph.nodes() ,label=True)
+    nx.draw_networkx_labels(graph, pos=poses)
+    plt.show()
 def random_graphs_init(graph,num):
-    poses = 0
-    vels = 1
+    poses = {i: (np.random.randint(i+10),np.random.randint(i+10)) for i in range(num)}
+    vels = {i: (0,0) for i in range(num)}
     nx.set_node_attributes(graph,poses, "pos")
     nx.set_node_attributes(graph, vels, "vel")
-    # poses.append(0)
     graph.add_edges_from([(i, i+1) for i in range(len(graph.nodes())-1)])
+    graph.add_edges_from([(i+1, i) for i in range(len(graph.nodes())-1)])
+    graph.add_edges_from([(num-1, 0), (0, num -1)])
 
-    edges = {(i, i+1):{graph.nodes[i+1]['pos'] - graph.nodes[i]['pos']}  for i in range(len(graph.nodes())-1) }
-    nx.set_edge_attributes(graph, edges, "edge_length")
     return graph
 
 def create_z_ref(num, int_type):
@@ -21,7 +31,7 @@ def create_z_ref(num, int_type):
    
     match int_type:
         case 1:
-             z_ref = np.linspace(0, num-1, num)
+             z_ref = np.linspace(0, num-1, 2*num)
         case 2:
              z_ref = [1,1,1,1,0,0,0,0]
         case _:
@@ -34,71 +44,44 @@ def single_xdot(x, t, D, z_ref,k):
     D_D = nx.incidence_matrix(D, oriented=True).toarray()
     return -k *np.matmul(L_D_bar, x) + k * np.matmul(D_D,z_ref)
 
-def double_xdot(x, t, D, z_ref,k):
-    k = 0.1
-    L_D_bar = nx.laplacian_matrix(D.to_undirected(reciprocal=False, as_view=False)).toarray()
-    D_D = k*nx.incidence_matrix(D, oriented=True).toarray()
-    
-    L_D_bar = -k *np.kron([[0,0], [1,1]] ,L_D_bar) + np.kron([[0,1], [0,0]], np.eye(len(x)//2))
-    
-   
-    D_D = k*np.kron([[0,0], [1,1]] ,D_D) 
-    
-    return np.matmul(L_D_bar, x) + np.matmul(D_D,z_ref)
+num = 5
+D =nx.empty_graph(num,create_using=nx.DiGraph())
+# D = nx.gnm_random_graph(num, (num -1)*(num-2)/2, directed=True)
+D = random_graphs_init(D,num)
 
-def LTI_xdot(x, t, D, z_ref,k):
-    a = .1
-    b = 1
-    z_t = np.matmul(nx.incidence_matrix(D, oriented=True).toarray().T, x)
-    u = k*np.matmul(nx.incidence_matrix(D, oriented=True).toarray(), (z_ref - z_t)) 
-    return a*x + b*u
 def main():
-    nums = [5]
+    
     k = 1
     
-    for num in nums:
-        labels = []
-        for i in range(num):
-            labels.append(f"x{i}")
-        D =nx.empty_graph(num,create_using=nx.DiGraph())
-        # D = nx.gnm_random_graph(num, (num -1)*(num-2)/2, directed=True)
-        D = random_graphs_init(D,num)
-        # z_ref = create_z_ref(len(D.edges()),1)
-        t = np.linspace(0, 30, 101)
-       
-        # trajectory_x = odeint(single_xdot, list(nx.get_node_attributes(D, "pos").values()), t, args=(D, z_ref, k))
-        # plt.figure()
-        # plt.plot(t, trajectory_x, label = labels)
-        # plt.xlabel("Time t")
-        # plt.ylabel("Position")
-        # plt.title("Single Integrater")
-        z_ref = create_z_ref(len(D.edges()),2)
-        print(z_ref)
-        pos_vel = []
-        for i in range(len(D.nodes())):
-            pos_vel.append(nx.get_node_attributes(D, "pos")[i])
-            pos_vel.append(nx.get_node_attributes(D, "vel")[i])
-        # pos_vel = np.append(list(nx.get_node_attributes(D, "pos").values()), list(nx.get_node_attributes(D, "vel").values()))
-        trajectory_double = odeint(double_xdot, pos_vel , t, args=(D, z_ref, k))
-        plt.figure()
-        plt.plot(t, trajectory_double[:,:num], label = labels)
-        plt.xlabel("Time t")
-        plt.ylabel("Position")
-        plt.title("Double Integrater")
-        plt.figure()
-        plt.plot(t, trajectory_double[:,num:], label = labels)
-        plt.xlabel("Time t")
-        plt.ylabel("Velocity")
-        plt.title("Double Integrater")
-        # z_ref = create_z_ref(len(D.edges()),3)
-        # trajectory_LTI = odeint(LTI_xdot, list(nx.get_node_attributes(D, "pos").values()) , t, args=(D, z_ref, k))
-        # plt.figure()
-        # plt.plot(t, trajectory_LTI, label = labels)
-        # plt.xlabel("Time t")
-        # plt.ylabel("Position")
-        # plt.title("LTI")
-    plt.legend()
+    labels = []
+    for i in range(num):
+        labels.append(f"x{i}")
+    
+    t = np.linspace(0, 10, 101)
+    ## Single
+    z_ref = create_z_ref(len(D.nodes()),1)
+    # np.append(list(nx.get_node_attributes(D, 'pos_x')),list(nx.get_node_attributes(D, 'pos_y')))
+    l=[]
+    [l.extend([v[0],v[1]]) for k,v in nx.get_node_attributes(D, 'pos').items()]
+    print(f"input for odeint: {l}")
+    trajectory_y = odeint(single_xdot,l, t, args=(D, z_ref, k))
+    plt.figure()
+    plt.plot(t, trajectory_y[:,:num], label = labels)
+    plt.xlabel("Time t")
+    plt.ylabel("Position")
+    plt.title("Double Integrater")
+    plt.figure()
+    plt.plot(t, trajectory_y[:,num:], label = labels)
+    plt.xlabel("Time t")
+    plt.ylabel("Velocity")
+    plt.title("Double Integrater")
+    plt.figure()
+    plt.plot( trajectory_y[:,num:],trajectory_y[:,:num], label = labels)
+    plt.plot(z_ref[num:], z_ref[:num])
+    plt.xlabel("Time t")
+    plt.ylabel("Velocity")
+    plt.title("Double Integrater")
+    show(D)
     plt.show()
-
 if __name__ == "__main__":
     main()
---
+++ HW4/HW4/temp.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+
+# Parameters
+num_robots = 5
+goal = np.array([10, 10])  # Goal position
+obstacles = np.array([[5, 5], [8, 12], [12, 7]])  # Obstacle positions
+k_goal = 1.0
+k_obst = 1.0
+k_form = 1.0
+
+# Define the function to compute the derivative of positions
+def compute_derivative(robot_positions, t):
+    # Ensure robot_positions has shape (num_robots, 2) and goal has shape (1, 2)
+    robot_positions = robot_positions.reshape(num_robots, 2)
+    goal_position = np.array([goal])  # Reshape goal to have shape (1, 2)
+    
+    # Compute forces
+    F_goal = -k_goal * (robot_positions - goal_position)
+    F_obst = np.zeros_like(robot_positions)
+    for obstacle in obstacles:
+        distance = robot_positions - obstacle
+        distance_norm = np.linalg.norm(distance, axis=1)
+        F_obst += k_obst * (distance / distance_norm[:, None]**3)
+    
+    # Formation maintenance forces
+    F_form = -k_form * (robot_positions - desired_formation_positions)
+    
+    # Compute derivatives
+    derivatives = F_goal + F_obst + F_form
+    return derivatives.flatten()
+
+# Initialize robot positions randomly
+robot_positions = np.random.rand(num_robots, 2) * 15
+
+# Define desired formation positions (e.g., linear formation)
+desired_formation_positions = np.array([[i, 0] for i in range(num_robots)])
+
+# Time array
+t = np.linspace(0, 10, 100)  # From 0 to 10 seconds with 100 time points
+
+# Integrate ODEs
+solution = odeint(compute_derivative, robot_positions.flatten(), t)
+solution = solution.reshape(-1, num_robots, 2)
+
+# Plotting
+for i in range(len(t)):
+    plt.figure()
+    plt.scatter(goal[0], goal[1], color='g', marker='x', label='Goal')
+    plt.scatter(obstacles[:, 0], obstacles[:, 1], color='r', marker='o', label='Obstacles')
+    plt.scatter(desired_formation_positions[:, 0], desired_formation_positions[:, 1], color='orange', marker='s', label='Desired Formation')
+    plt.scatter(solution[i, :, 0], solution[i, :, 1], color='b', marker='o', label='Robots')
+    plt.title(f'Time: {t[i]:.2f}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.legend()
+    plt.grid(True)
+    plt.axis('equal')
+plt.show()
