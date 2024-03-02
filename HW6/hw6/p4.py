e504fc3013b5c49371b56bd81cdfd39544666e84
soowhanyi <soowhanyi94@gmail.com>
2024-03-01 17:12:20 -0800

    done

--- HW6/hw6/p4.py
+++ HW6/hw6/p4.py
@@ -11,31 +11,45 @@ def main():
     for j in range(10):
 
         weight = {(0,1): 1+j, (1,2): 1}
-        iteration = 1000 #number of iteration to simulate limit
+        iteration = 100 #number of iteration to simulate limit
         time_lim = iteration *delta
         nx.set_edge_attributes(graph, weight, 'weight')
         max_eigen_val = np.max(nx.laplacian_spectrum(graph, weight='weight'))
         L_w_G = nx.laplacian_matrix(graph, weight='weight').toarray()
         del_max_eig = round(delta*max_eigen_val,4)
-        print(f"delta* max eigen value = {del_max_eig}")
         M_G = np.eye(num) - delta*L_w_G
         true_value = np.random.randint(0, 100, size = num)
         avg_true_value = np.average(true_value)
         H =  np.eye(num)
         estimates = []
+        P_mat = []
         t = np.linspace(0, time_lim, iteration)
-        print(list(weight.values()))
         for i in range(num):
             rand_noise = np.random.normal(0,1, size = num)
             z = H@true_value + rand_noise
+            P = np.eye(9)
             for k in range(iteration):
                 z = M_G @z
+                P = np.kron(M_G,np.eye(3)) @P
                 estimates.append(z)
-            plt.figure(j)
+                P_mat.append(P)
+            plt.figure(2*j)
             plt.plot(t, estimates)
             plt.plot(t, [avg_true_value]*iteration)
+            plt.xlabel("time t")
+            plt.ylabel(r"$\hat \theta (t)$")
             plt.title(r"$\Delta  \rho (L(G)) =" f"{del_max_eig}$" ", weight = " f"{list(weight.values())}" f" $\Delta = {delta}$")
             estimates = []
+            plt.figure(2*j+1)
+            P_mat_plot = []
+            for k in range(iteration):
+                P_mat_k = P_mat[k]
+                P_mat_plot.append(np.array(np.diag(P_mat_k)))
+            plt.plot(t, P_mat_plot)
+            plt.xlabel("time t")
+            plt.ylabel(f"$P_i (t)$")
+            plt.title(r"$\Delta  \rho (L(G)) =" f"{del_max_eig}$" ", weight = " f"{list(weight.values())}" f" $\Delta = {delta}$")
+            P_mat = []
         plt.show()
 
 if __name__ == "__main__":
--- final_project/final_project/final_project.py
+++ final_project/final_project/final_project.py
@@ -5,26 +5,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 
-#https://en.wikipedia.org/wiki/Gram%E2%80%93Schmidt_process
-def gramschmidt(V):
-    n, m = V.shape
-    U = np.zeros_like(V)
-    U[:, 0] = V[:, 0] / np.linalg.norm(V[:, 0])
-    for i in range(1, m):
-        U[:, i] = V[:, i]
-        for j in range(i):
-            U[:, i] -= np.dot(U[:, j].T, U[:, i]) * U[:, j]
-        U[:, i] /= np.linalg.norm(U[:, i])
-    return U[:,4:]
-def create_formation(num):
+
+
+def P_dot(p, t, graph, P, num, dim):
+    D_D = nx.incidence_matrix(graph, oriented=True).toarray()
+    z_ref = np.kron(D_D.T, np.eye(2))@ P
+    z_t = np.kron(D_D.T, np.eye(2))@p
+    u = np.kron(D_D, np.eye(2)) @(z_ref - z_t)
+    
+    if (t>9) & (t < 9.2):
+        p_reshape = np.reshape(p, (num,dim))
+        pos = {i:(p_reshape[i-1]) for i in range(1, num+1)}
+        nx.set_node_attributes(graph, pos, "pos")
+        nx.draw(graph, pos = pos)
+        plt.show()
+    return u
+def create_formation(num, dim, P, P_bar):
     graph = nx.empty_graph()
     graph.add_nodes_from([i for i in range(1, num + 1)])
     graph.add_edges_from([(1,2), (1,4), (1,5), (1,6), (2,3), (2,5), (2,8), (3,4), (4, 6),(4,7),(5,6),(5,7), (6,8),(7,8)])
-    nx.set_node_attributes(graph, {i: np.random.randint(0, 10, 3) for i in range(1, num+1)}, name="pos")
+    nx.set_node_attributes(graph, {i: np.random.randint(0, 10, 2) for i in range(1, num+1)}, name="pos")
     number_of_edges = len(graph.edges)
     # r = np.array([np.array([0,0]).T,np.array([-1,1]).T,np.array([-1,0]).T,np.array([-1,-1]).T,np.array([-2,1]).T,np.array([-2,-1]).T,np.array([-3,1]).T,np.array([-3,-1]).T])
     # graph.add_edges_from([(1,1)])
-    P_bar = np.array(np.column_stack([[[0,0],[-1,1],[-1,0],[-1,-1],[-2,1],[-2,-1],[-3,1],[-3,-1]], np.ones(num)]))
+    
+    
     D_D = nx.incidence_matrix(graph, oriented=True).toarray()
     E = [[]]
     
@@ -36,8 +41,7 @@ def create_formation(num):
             E = np.row_stack([E,P_bar.T@ D_D @np.diag(D_D.T[:,i])])
     z = sp.linalg.null_space(E)
     U, s, VT = sp.linalg.svd(P_bar)
-    d = 2
-    U_2 = U[:,d+1:]
+    U_2 = U[:,dim+1:]
     M = []
     omega = []
     Omega = cp.Variable((z.shape[0], z.shape[0]), diag=True)
@@ -49,14 +53,41 @@ def create_formation(num):
         else:
             omega += cp.multiply(c[i], z[:,i])
             # M += cp.multiply(c[i],U_2.T @ D_D @np.diag(z[:,i]) @ D_D.T @ U_2)
-    print(cp.diag(omega))
-    gamma = cp.Variable(1)
-    objective = cp.Maximize(gamma)
+    
     constraints = [cp.trace(cp.diag(omega)) == 1, U_2.T @ D_D @cp.diag(omega) @ D_D.T @ U_2 >>0]
     prob = cp.Problem(objective=cp.Minimize(0), constraints=constraints)
     result = prob.solve()
-    print(omega.value)
-    # 
+    edges = list(graph.edges())
+    edge_labels = [ '%.4f' % elem for elem in omega.value]
+    weight = {edges[i]: edge_labels[i] for i in range(len(edges))}
+    pos = nx.get_node_attributes(graph,"pos")
+    nx.draw(graph,pos = pos, with_labels = True)            
+    nx.draw_networkx_edge_labels(graph, pos = pos, edge_labels=weight)
+    plt.show()
+    return graph, weight, pos
+
+def simulate(graph, num, dim, P):
+    delta = .1
+    iteration = 1000 #number of iteration to simulate limit
+    time_lim = iteration *delta
+    t = np.linspace(0, time_lim, iteration)
+    p = np.reshape(list(nx.get_node_attributes(graph,"pos").values()), (num*dim))
+    trajectory_P = sp.integrate.odeint(P_dot,p, t, args = (graph, P , num, dim))
+    print(trajectory_P.shape)
+    plt.figure()
+    labels = []
+    for i in range(num):
+        labels.append(f"x{i}")
+    t = np.linspace(0, time_lim, iteration)
+    # pic_frame = w
+    for i in range(num):
+
+        plt.plot(trajectory_P[990,2*i], trajectory_P[980,2*i +1], 'ko')
+        plt.plot(trajectory_P[:,2*i], trajectory_P[:,2*i +1], label =labels[i])
+        plt.xlabel("Time t")
+        plt.ylabel("Position")
+    plt.legend()
+    plt.show()
     L  = nx.laplacian_matrix(graph).toarray()
     # W = cp.Variable((number_of_edges,number_of_edges), symmetric = True)
     # constraints = [W >> 1]
@@ -71,10 +102,14 @@ def create_formation(num):
     # result = prob.solve()
     # print(D_D)
 
-    return graph
 def main():
     num = 8
-    graph = create_formation(num)
+    dim = 2
+    P = [[0,0],[-1,1],[-1,0],[-1,-1],[-2,1],[-2,-1],[-3,1],[-3,-1]]
+    P = np.reshape(P, num*dim)
+    P_bar = np.array(np.column_stack([[[0,0],[-1,1],[-1,0],[-1,-1],[-2,1],[-2,-1],[-3,1],[-3,-1]], np.ones(num)]))
+    graph, weight_dic, pos_dic = create_formation(num, dim, P, P_bar)
+    simulate(graph, num, dim, P)
     name = 'custom'
     
 
